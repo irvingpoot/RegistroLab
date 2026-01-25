902dc1e2aaa6638e90f658bc3f82236db0f3fc2c
Irving Poot <eduardopoot17@gmail.com>
2026-01-24 22:08:10 -0600

    Add patient type separation and mood patient flows
    Introduces patient type selection modal and separates patient registration and editing into 'sueño' and 'ánimo' flows.
    
    Adds new pages and logic for mood (ánimo) patients, updates dashboard and list to support and display patient types, and adjusts protected routes and changelog for the new features.
    
    Also fixes Berlin scale logic and improves UI/UX for related components.

--- src/data/changelog.ts
+++ src/data/changelog.ts
@@ -1,14 +1,34 @@
-export const currentVersion = "1.2.1";
+export const currentVersion = "1.3.0";
 
-export const isMajorUpdate = false; 
+export const isMajorUpdate = true; 
 
 export const updateDate = "26 de Enero, 2026";
 
 // Opciones type: 'feature', 'fix', 'style'
 export const changes = [
     {
+        title: "Tipos de paciente",
+        description: "Se agregó la funcionalidad para separar el registro de pacientes de sueño y ánimo.",
+        type: "feature"
+    },
+    {
+        title: "Sesiones TMS",
+        description: "Se agregó la funcionalidad para gestionar sesiones de TMS para pacientes candidatos.",
+        type: "feature"
+    },
+    {
+        title: "Consentimiento TMS",
+        description: "Se agregó la funcionalidad para gestionar el almacenamiento del consentimiento informado para sesiones de TMS.",
+        type: "feature"
+    },
+    {
+        title: "IMC en Berlín",
+        description: "Se corrgió el algoritmo de la categoría 3 en la escala de Berlin para no considerar el IMC.",
+        type: "fix"
+    },
+    {
         title: "Iconos actualizados",
         description: "Se actualizaron los iconos de los tickets para evitar el desbordamiento de los mismos en /feedback.",
         type: "style"
-    },
+    }
 ];
